1a38c8dd564f089f133df0e83a85561e56da691b
Elmira Bayramova <123525890+lobdob@users.noreply.github.com>
2023-05-09 21:32:14 -0400

    Update FirstClass.java

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -3,5 +3,7 @@ package firstPackage;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("First Class");
+        //Some changes are made
+         System.out.println("First Class");
     }
 }
