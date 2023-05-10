77231d07cea30c1518d9597495433d22ceacc2bf
Elmira Bayramova <123525890+lobdob@users.noreply.github.com>
2023-05-09 21:36:46 -0400

    Update SecondClass.java

--- src/firstPackage/SecondClass.java
+++ src/firstPackage/SecondClass.java
@@ -3,5 +3,7 @@ package firstPackage;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Second Class");
+        
+        //made some changes
     }
 }
